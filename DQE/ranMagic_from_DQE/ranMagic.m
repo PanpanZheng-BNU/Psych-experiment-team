52f363a65a37fb825cb811762e9b936862520dd7
zppdezhanghao <45086526+zppdezhanghao@users.noreply.github.com>
2019-04-15 14:28:56 +0800

    2

--- DQE/ranMagic_from_DQE/ranMagic.m
+++ DQE/ranMagic_from_DQE/ranMagic.m
@@ -1,8 +1,8 @@
 %function name is ranMagic
 %命令框中输入“ranMagic”可以得到一个3x3的随机魔方矩阵
-function A=ranMagic(n)  %%%函数最好能自定义自变量；
+function A=ranMagic
 %获得随机魔方矩阵
-B=magic(n);
+B=magic(3);
 %获得其他7中形式的3x3随机魔方矩阵
 C=B';
 D=rot90(B);
@@ -16,8 +16,5 @@ J=[B C D E F G H I];
 %随机生成一个数字以代表选取的矩阵
 o=unidrnd(8);
 %将随机的矩阵赋值给A
-A=[];
-for ii=1:n
-    A=[A,J(:,n*o-ii+1)];
-end
+A=[J(:,3*o-2) J(:,3*o-1) J(:,3*o)];
 end
